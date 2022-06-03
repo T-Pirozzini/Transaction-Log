22090a99be06fc9236b58c7b1fe7382913469524
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-06-03 11:05:11 -0700

    implement interface

--- public/app.js
+++ public/app.js
@@ -1,16 +1,13 @@
-const me = {
-    name: 'Travis',
-    age: 30,
-    speak(text) {
-        console.log(text);
-    },
-    spend(amount) {
-        console.log('I spent', amount);
-        return amount;
-    }
-};
-console.log(me);
 import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+let docOne;
+let docTwo;
+docOne = new Invoice('yoshi', 'web work', 250);
+docTwo = new Payment('mario', 'plumbing work', 200);
+let docs = [];
+docs.push(docOne);
+docs.push(docTwo);
+console.log(docs);
 const invOne = new Invoice('Ben', 'Owes for Pizza last Tuesday', 15);
 const invTwo = new Invoice('Chun-li', 'Owes for fighting lessons', 250);
 let invoices = [];
@@ -27,5 +24,12 @@ const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
 });
