4ba1889585f6eff52d2c1ce0625dbc59438c6d30
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-06-03 11:51:45 -0700

    generics and enums tutorial

--- public/app.js
+++ public/app.js
@@ -21,3 +21,42 @@ form.addEventListener('submit', (e) => {
     }
     list.render(doc, type.value, 'end');
 });
+// EXTRA INFORMATION - Tutorial
+// GENERICS
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docFive = addUID({ name: 'yoshi', age: 40 });
+console.log(docFive.age);
+const docThree = {
+    uid: 1,
+    resourceName: 'person',
+    data: { name: 'Travis' }
+};
+const docFour = {
+    uid: 2,
+    resourceName: 'shoppingList',
+    data: ['bread', 'milk', 'toilet roll']
+};
+console.log(docThree, docFour);
+// ENUMS
+var ResourceType;
+(function (ResourceType) {
+    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
+    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
+    ResourceType[ResourceType["FILM"] = 2] = "FILM";
+    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
+    ResourceType[ResourceType["PERSON"] = 4] = "PERSON";
+})(ResourceType || (ResourceType = {}));
+const docOne = {
+    uid: 1,
+    resourceType: ResourceType.BOOK,
+    data: { title: 'name of the wind' }
+};
+const docTwo = {
+    uid: 10,
+    resourceType: ResourceType.PERSON,
+    data: { name: 'yoshi' }
+};
+console.log(docOne, docTwo);
--- src/app.ts
+++ src/app.ts
@@ -27,3 +27,61 @@ form.addEventListener('submit', (e: Event) => {
 
   list.render(doc, type.value, 'end')  
 })
+
+
+// EXTRA INFORMATION - Tutorial
+
+// GENERICS
+const addUID = <T extends {name: string}>(obj: T) => {
+  let uid = Math.floor(Math.random() * 100);
+  return {...obj, uid};
+}
+
+let docFive = addUID({name: 'yoshi', age: 40});
+
+console.log(docFive.age)
+
+// generics with interfaces
+interface TResource<T> {
+  uid: number;
+  resourceName: string;
+  data: T;
+}
+
+const docThree: TResource<object> = {
+  uid: 1,
+  resourceName: 'person',
+  data: { name: 'Travis' }
+}
+
+const docFour: TResource<string[]> = {
+  uid: 2,
+  resourceName: 'shoppingList',
+  data: ['bread', 'milk', 'toilet roll']
+}
+
+console.log(docThree, docFour)
+
+
+// ENUMS
+enum ResourceType { BOOK, AUTHOR, FILM, DIRECTOR, PERSON }
+
+interface Resource<T> {
+  uid: number;
+  resourceType: ResourceType;
+  data: T;
+}
+
+const docOne: Resource<object> = {
+  uid: 1, 
+  resourceType: ResourceType.BOOK,
+  data: { title: 'name of the wind'}
+}
+
+const docTwo: Resource<object> = {
+  uid: 10, 
+  resourceType: ResourceType.PERSON,
+  data: { name: 'yoshi'}
+}
+
+console.log(docOne, docTwo)
